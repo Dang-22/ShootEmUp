1596620c71a8fb108385c56f5711d4474487b0ec
HungDang <84671773+Dang-22@users.noreply.github.com>
2024-12-26 23:15:46 +0700

    ~Remake enemy pool

--- Assets/ShootEmUp/Scripts/EnemySpawner.cs
+++ Assets/ShootEmUp/Scripts/EnemySpawner.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Pool;
 using UnityEngine.Splines;
 using Random = UnityEngine.Random;
 
@@ -13,45 +14,49 @@ namespace ShootEmUp
     {
         [SerializeField] private List<EnemyType> _enemiesTypes;
         [SerializeField] private int _maxEnemies = 18;
-        [SerializeField] private float _spawnInterval = 2f;
-        private EnemyPool _enemyPool;
+        [SerializeField] private float _spawnInterval = 1f;
+        [SerializeField] private GameObject _poolHoldder;
         private List<SplineContainer> _spline;
         private EnemyFactory _enemyFactory;
         private float _spawnTimer;
         private int _enemySpawned;
+        private ObjectPool<GameObject> _pool;  
         /// <summary>
         /// 
         /// </summary>
-        private void OnValidate()
+        private void Awake()
         {
             //Get the component 
+            _enemyFactory = new EnemyFactory();
             _spline = new List<SplineContainer>(GetComponentsInChildren<SplineContainer>());
-        }
-        private void Start()
-        {
-            _enemyPool = gameObject.AddComponent<EnemyPool>();
-            _enemyPool.InitializeEnemyPool(_enemiesTypes[0], _spline[Random.Range(0, _spline.Count)]);
+            _pool = new ObjectPool<GameObject>(SpawnEnemy, null, OnPutBackInPool, defaultCapacity: 200);
         }
 
+        private void OnPutBackInPool(GameObject obj)
+        {
+            obj.gameObject.SetActive(false);
+        }
         private void Update()
         {
             //Spawn the enemy based on time and quantities
             _spawnTimer += Time.deltaTime;
             if (_enemySpawned < _maxEnemies && _spawnTimer >= _spawnInterval)
             {
-                SpawnEnemy();
+               GameObject _poolHoldderItem = _pool.Get();
+                _poolHoldderItem.transform.SetParent(_poolHoldder.transform);
                 _spawnTimer = 0f;
             }
         }
         /// <summary>
         /// Create the enemy with factory setting
         /// </summary>
-        private void SpawnEnemy()
+        private GameObject SpawnEnemy()
         {
             EnemyType enemyType = _enemiesTypes[Random.Range(0, _enemiesTypes.Count)];
             SplineContainer spline = _spline[Random.Range(0, _spline.Count)];
-            _enemyPool.GetEnemy(enemyType, spline);
+            GameObject enemy = _enemyFactory.CreateEnemy(enemyType, spline);
             _enemySpawned++;
+            return enemy;
         }
     }
 }
