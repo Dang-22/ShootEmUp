bdd19073c9afd41000b7dfc02ccc8e144bb4b122
HungDang <84671773+Dang-22@users.noreply.github.com>
2024-12-30 14:25:36 +0700

    ! Add method, change to WeaponBase base

--- Assets/ShootEmUp/Scripts/DoubleShot.cs
+++ Assets/ShootEmUp/Scripts/DoubleShot.cs
@@ -1,23 +1,23 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace ShootEmUp
 {
     [CreateAssetMenu(fileName = "DoubleShot", menuName = "ShootEmUp/WeaponStrategy/DoubleShot", order = 1)]
     public class DoubleShot : WeaponStrategy
     {
+        #region Fieds
+
         [SerializeField] private float _spreadAngle = 15f;
+        private List<GameObject> _projectiles = new List<GameObject>();
+
+        #endregion
+        #region Public Methods
         public override void Fire(Transform firePoint, LayerMask layer)
         {
-            for (int i = 0; i < 2; i++)
-            {
-                var projectile = Instantiate(_projectilePrefab, firePoint.position, firePoint.rotation);
-                projectile.transform.SetParent(firePoint);
-                projectile.layer = layer;
-                projectile.transform.Rotate(0f, _spreadAngle * (i - 1), 0f);
-                var projecttileComponent = projectile.GetComponent<ProjectTile>();
-                projecttileComponent.SetSpeed(_projecttileSpeed);
-                Destroy(projectile, _projecttileLifetime);
-            }
+            SpawnProjectile(firePoint, layer, -_spreadAngle);
+            SpawnProjectile(firePoint, layer, _spreadAngle);
         }
+        #endregion
     }
 }
--- Assets/ShootEmUp/Scripts/SingleShot.cs
+++ Assets/ShootEmUp/Scripts/SingleShot.cs
@@ -6,6 +6,8 @@ namespace ShootEmUp
     //Fire one at the time  
     public class SingleShot : WeaponStrategy
     {
+        #region Public Methods
+
         /// <summary>
         /// Set projecttile's value
         /// </summary>
@@ -13,12 +15,9 @@ namespace ShootEmUp
         /// <param name="layer"></param>
         public override void Fire(Transform firePoint, LayerMask layer)
         {
-            var projectile = ProjecttilePool.Instance.GetProjectile();
-            projectile.transform.position = firePoint.position;
-            projectile.transform.rotation = firePoint.rotation;
-            projectile.layer = layer;
-            var projecttileComponent = projectile.GetComponent<ProjectTile>();
-            projecttileComponent.SetSpeed(_projecttileSpeed);
+            SpawnProjectile(firePoint, layer, 0f);
         }
+
+        #endregion
     }
 }
