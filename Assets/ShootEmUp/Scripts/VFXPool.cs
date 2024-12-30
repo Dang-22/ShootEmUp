215ffdf4784b5ad357f22cd11d548b0e082eae0b
HungDang <84671773+Dang-22@users.noreply.github.com>
2024-12-30 14:28:20 +0700

    + VFX Pool

---
+++ Assets/ShootEmUp/Scripts/VFXPool.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Pool;
+
+namespace ShootEmUp
+{
+    public class VFXPool : MonoBehaviour
+    {
+        #region Fields
+        [SerializeField] private GameObject _vfxPrefab;
+        private ObjectPool<GameObject> _vfxPool;
+        public static VFXPool Instance { get; private set; }
+        #endregion
+        #region Unity Methods
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+            else
+            {
+                Destroy(gameObject);
+                return;
+            }
+            _vfxPool =
+                new ObjectPool<GameObject>(VFXSpawn, OnPullOutOfPool, OnPutBackInPool, defaultCapacity: 100);
+        }
+        #endregion
+        #region Private Method
+
+        private void OnPullOutOfPool(GameObject obj)
+        {
+            obj.SetActive(true);
+        }
+
+        private void OnPutBackInPool(GameObject obj)
+        {
+            obj.SetActive(false);
+        }
+        private GameObject VFXSpawn()
+        {
+            var instance = Instantiate(_vfxPrefab);
+            instance.transform.SetParent(gameObject.transform);
+            return instance;
+        }
+
+        #endregion
+        #region Public Method
+        public GameObject GetVFX(Transform parent)
+        {
+            GameObject fx = _vfxPool.Get();
+            fx.transform.position = parent.position;
+            return fx;
+        }
+        public void ReleaseVFX(GameObject vfx)
+        {
+            _vfxPool.Release(vfx);
+        }
+        #endregion
+    }
+}
