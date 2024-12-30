be5d3bf676bb6526541d30d2efd3cb4f20420bed
HungDang <84671773+Dang-22@users.noreply.github.com>
2024-12-30 14:20:09 +0700

    ~Resetting properties

---
+++ Assets/ShootEmUp/Scripts/VFXBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+namespace ShootEmUp
+{
+    public class VFXBehavior : MonoBehaviour
+    {
+        #region Fields
+
+        [SerializeField] private ParticleSystem _particleSystem;
+        
+
+        #endregion
+        #region Unity Methods
+
+        private void Update()
+        {
+            if (_particleSystem != null)
+            {
+                if (_particleSystem.isStopped)
+                {
+                    VFXPool.Instance.ReleaseVFX(gameObject);
+                }
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/ShootEmUp/Scripts/WeaponBase.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+namespace ShootEmUp
+{
+    /// <summary>
+    /// Fire style
+    /// </summary>
+    public abstract class WeaponBase : Weapon
+    {
+        #region Fields
+
+        private float _fireTimer;
+        
+
+        #endregion
+        #region Unity Methods
+
+        protected virtual void Update()
+        {
+            HandleFiring();
+        }
+
+        #endregion
+        #region FireOneShot
+
+        /// <summary>
+        /// Auto fire after _fireTimer time
+        /// </summary>
+        private void HandleFiring()
+        {
+            _fireTimer += Time.deltaTime;
+
+            if (_fireTimer >= _weaponStrategy.FireRate)
+            {
+                _weaponStrategy.Fire(_firePoint, _layer);
+                _fireTimer = 0f;
+            }
+        }
+
+        #endregion
+    }
+}
